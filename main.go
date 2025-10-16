477379aa20a44853641169e0a77cb38129cd12c1
frankostasur <frankostasur@gmail.com>
2025-10-16 12:50:08 +0300

    updates on package os to write text file

--- main.go
+++ main.go
@@ -2,20 +2,44 @@ package main
 
 import (
 	"fmt"
+	"errors"
+	"os"
 
 )
 
 func main() {
-	revenue := getUserInput("Enter your revenue:")
-	expenses := getUserInput("Enter your expences:")
-	taxRate := getUserInput("Enter your taxRate:")
+	revenue, err:= getUserInput("Enter your revenue:")
 
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+
+	expenses, err := getUserInput("Enter your expences:")
+
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+
+	taxRate, err := getUserInput("Enter your taxRate:")
+
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
 
 	EBT, profit, ratio :=calculatevalues(revenue, expenses, taxRate)
 
 	fmt.Println(EBT)
 	fmt.Println(profit)
 	fmt.Println(ratio)
+	storeResults(EBT, profit, ratio)
+}
+func storeResults(EBT, profit, ratio float64)  {
+	results := fmt.Sprintf("EBT: %0.1f\n Profit: %0.1f\n Ratio: %0.3f\n", EBT, profit, ratio)
+	os.WriteFile("results.txt", []byte (results), 0644)
+	
 }
 func calculatevalues(revenue, expenses, taxRate float64) (float64 ,float64, float64){
 	EBT := revenue - expenses
@@ -23,10 +47,14 @@ func calculatevalues(revenue, expenses, taxRate float64) (float64 ,float64, floa
 	ratio := EBT/profit
 	return EBT, profit ,ratio
 }
-func getUserInput(infoText string) float64{
+func getUserInput(infoText string) (float64, error){
 	var userInput float64
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
-	return userInput
+
+	if userInput <= 0 {
+		return 0, errors.New("value must be a positive number.")
+	}
+	return userInput, nil
 
 }
